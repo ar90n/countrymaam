24e23ec916ba0507f8a74990eda93daeeb7b8c07
Masahiro Wada <argon.argon.argon@gmail.com>
2022-01-27 13:38:23 +0000

    update benchmark script

--- benchmark/countrymaam.py
+++ benchmark/countrymaam.py
@@ -9,7 +9,8 @@ import sys
 import os
 import glob
 import numpy as np
-from pathlib import Path
+import random
+import string
 
 class Countrymaam(BaseANN):
     def __init__(self, metric, params):
@@ -20,6 +21,8 @@ class Countrymaam(BaseANN):
 
     def fit(self, X):
         X = X.astype(np.float64)
+        suffix = "".join(random.choices(string.ascii_lowercase, k=16))
+        index_file_path = f"index_{suffix}_{os.getpid()}.bin"
         p = subprocess.Popen([
             "countrymaam",
             "train",
@@ -27,6 +30,7 @@ class Countrymaam(BaseANN):
             "--index", self._index,
             "--leaf-size", str(self._leaf_size),
             "--tree-num", str(self._n_trees),
+       	    "--output", index_file_path
         ], stdin=subprocess.PIPE)
 
         p.stdin.write(struct.pack(f"={X.size}d", *np.ravel(X)))
@@ -38,6 +42,7 @@ class Countrymaam(BaseANN):
             "predict",
             "--dim", str(len(X[0])),
             "--index", self._index,
+       	    "--input", index_file_path
         ], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
     def set_query_arguments(self, search_k):
@@ -45,16 +50,16 @@ class Countrymaam(BaseANN):
 
     def query(self, v, n):
         v = v.astype(np.float64)
-        self._p.stdin.write(struct.pack(f"=i", self._search_k))
-        self._p.stdin.write(struct.pack(f"=i", n))
-        self._p.stdin.write(struct.pack(f"={v.size}d", *v))
-        self._p.stdin.flush()
+        self._pipe.stdin.write(struct.pack(f"=i", self._search_k))
+        self._pipe.stdin.write(struct.pack(f"=i", n))
+        self._pipe.stdin.write(struct.pack(f"={v.size}d", *v))
+        self._pipe.stdin.flush()
 
-        rn = struct.unpack("=i", self._p.stdout.read(4))[0]
+        rn = struct.unpack("=i", self._pipe.stdout.read(4))[0]
         ret = [0] * rn
         for i in range(rn):
-            ret[i] = struct.unpack("=i", self._p.stdout.read(4))[0]
+            ret[i] = struct.unpack("=i", self._pipe.stdout.read(4))[0]
         return np.array(ret)
 
     def __str__(self):
-        return f"Countrymaam(index={self._index_name}, leaf_size={self._leaf_size} n_trees={self._n_trees}, search_k={self._search_k})"
+        return f"Countrymaam(index={self._index}, leaf_size={self._leaf_size} n_trees={self._n_trees}, search_k={self._search_k})"
